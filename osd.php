68ae33c180fadcf311b93f2a966da68786614c91
Evert Pot <evert@rooftopsolutions.nl>
2012-01-21 18:47:42 +0100

    Moved to a separate xml file

--- osd.php
+++ osd.php
@@ -2,22 +2,4 @@
 
 header('Content-Type: application/opensearchdescription+xml');
 
-echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
-?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"  
-                       xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-
-    <ShortName>RFC search</ShortName>
-    <Description>Quickly look up RFC's</Description>
-    <InputEncoding>UTF-8</InputEncoding>
-	<AdultContent>false</AdultContent>
-	<Developer>Evert Pot</Developer>
-    <Contact>evert@rooftosolutions.nl</Contact>
-    <SyndicationRight>open</SyndicationRight>
-    <Image width="16" height="16">http://rfcsearch.phpfogapp.com/rfc.png</Image>
-    <Url type="text/html" method="GET" template="http://rfcsearch.phpfogapp.com/search.php">  
-        <Param name="q" value="{searchTerms}"/>
-    </Url> 
-    <!-- <Url type="application/x-suggestions+json" method="GET" template="http://rfcsearch.phpfogapp.com/suggest.php?q={searchTerms}"/>-->
-    <moz:SearchForm>http://rfcsearch.phpfogapp.com/search.php</moz:SearchForm> 
-</OpenSearchDescription>
+readfile('osd.xml');
