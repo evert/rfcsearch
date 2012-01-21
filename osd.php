32d54f4a4e308bc5441dd3b457ae2b95fdd04f0d
Evert Pot <evert@rooftopsolutions.nl>
2012-01-21 18:30:50 +0100

    New osd

--- osd.php
+++ osd.php
@@ -2,13 +2,12 @@
 
 header('Content-Type: application/opensearchdescription+xml');
 
-echo '<?xml version="1.0"?>' . "\n";
+echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 ?>
-<OpenSearchDescription 
-    xmlns="http://a9.com/-/spec/opensearch/1.1/" 
-    xmlns:moz="http://www.mozilla.org/2006/browser/search/"> 
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"  
+                       xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+
     <ShortName>RFC search</ShortName>
-	<LongName>IETF RFC search </LongName>
     <Description>Quickly look up RFC's</Description>
     <InputEncoding>UTF-8</InputEncoding>
 	<AdultContent>false</AdultContent>
@@ -16,9 +15,9 @@ echo '<?xml version="1.0"?>' . "\n";
     <Contact>evert@rooftosolutions.nl</Contact>
     <SyndicationRight>open</SyndicationRight>
     <Image width="16" height="16">http://tools.ietf.org/images/rfc.png</Image>
-    <Url type="text/html" method="GET" template="http://rfcsearch.phpfogapp.com/search.php">
-		<Param name="q" value="{searchTerms}"/>
-	</Url>
-    <Url type="application/x-suggestions+json" method="GET" template="http://rfcsearch.phpfogapp.com/suggest.php?q={searchTerms}"/>
+    <Url type="text/html" method="GET" template="http://rfcsearch.phpfogapp.com/search.php">  
+        <Param name="q" value="{searchTerms}"/>
+    </Url> 
+    <!-- <Url type="application/x-suggestions+json" method="GET" template="http://rfcsearch.phpfogapp.com/suggest.php?q={searchTerms}"/>-->
     <moz:SearchForm>http://rfcsearch.phpfogapp.com/search.php</moz:SearchForm> 
 </OpenSearchDescription>
