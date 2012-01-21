d783790c611ed52aab6f88c08477f08df56f75ef
Evert Pot <evert@rooftopsolutions.nl>
2012-01-21 18:43:49 +0100

    Added a bunch more keywords

--- keywordlist.php
+++ keywordlist.php
@@ -1,8 +1,40 @@
 <?php
 
+/*
+ * The fallback for search providers is to use google, but google doesn't 
+ * always get it right, and is slower.
+ *
+ * This is a simple default list of keywords and what rfc they should map to. 
+ * Feel free to submit pull requests for more.
+ */
 return array(
-    'itip' => 5546,
-    'imip' => 6047,
+
+    'atom'       => 4287,
+    'atompub'    => 5023,
+
+    'basic'      => 2617,
+    'basic auth' => 2617,
+
+    'caldav'     => 4791,
+    'carddav'    => 6352,
+
+    'cookie'     => 2965,
+
+    'digest'     => 2617,
+    'digest auth'=> 2617,
+
+    'http'       => 2616,
+    'http auth'  => 2617,
+
+    'icalendar'  => 5545,
+    'imip'       => 6047,
+    'itip'       => 5546,
+
+    'smtp'       => 2821,
+
+    'vcard'      => 6350,
+
+    'websocket'  => 6455,
 
 );
 
