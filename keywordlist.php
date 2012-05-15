393507b0347176a976b8df1ef0cbbecddc333227
Evert Pot <evert@rooftopsolutions.nl>
2012-05-15 19:36:16 +0200

    Added webdav deltav

--- keywordlist.php
+++ keywordlist.php
@@ -9,36 +9,37 @@
  */
 return array(
 
-    'atom'       => 4287,
-    'atompub'    => 5023,
+    'atom'           => 4287,
+    'atompub'        => 5023,
 
-    'basic'      => 2617,
-    'basic auth' => 2617,
+    'basic'          => 2617,
+    'basic auth'     => 2617,
 
-    'caldav'     => 4791,
-    'carddav'    => 6352,
+    'caldav'         => 4791,
+    'carddav'        => 6352,
 
-    'cookie'     => 2965,
+    'cookie'         => 2965,
 
-    'digest'     => 2617,
-    'digest auth'=> 2617,
+    'digest'         => 2617,
+    'digest auth'    => 2617,
 
-    'http'       => 2616,
-    'http auth'  => 2617,
+    'http'           => 2616,
+    'http auth'      => 2617,
 
-    'icalendar'  => 5545,
-    'imip'       => 6047,
-    'itip'       => 5546,
+    'icalendar'      => 5545,
+    'imip'           => 6047,
+    'itip'           => 5546,
 
-    'mkcol'      => 5689,
+    'mkcol'          => 5689,
 
-    'smtp'       => 2821,
+    'smtp'           => 2821,
 
-    'vcard'      => 6350,
+    'vcard'          => 6350,
 
-    'websocket'  => 6455,
-    'webdav acl' => 3744,
-    'webdav sync'=> 6578,
+    'websocket'      => 6455,
+    'webdav acl'     => 3744,
+    'webdav deltav'  => 3253,
+    'webdav sync'    => 6578,
 
 );
 
